2eb732eb3ff356e64da61848cef0e6f5879679e2
Манаков Владислав <vsmanakov@sberbank.ru>
2021-09-13 06:54:55 +0300

    Добавляет комментарии

--- src/main/java/com/java_shop/Goods/GoodsService.java
+++ src/main/java/com/java_shop/Goods/GoodsService.java
@@ -2,11 +2,13 @@ package com.java_shop.Goods;
 
 // в сервисах видимо будет выполняться обращение к другим сервисам для агрегации данных
 public class GoodsService {
-    public static Good saveGood(String name, int price) {
+    public static Good saveGood(GoodCreate goodCreate) {
 
         // Тут ли должен создаваться объект Good и как сгенерить id?
-        Good good = new Good(1, name, price);
+        Good good = new Good(1, goodCreate.getName(), goodCreate.getPrice());
 
         return GoodsRepository.add(good);
     }
 }
+
+// GoodCreate
